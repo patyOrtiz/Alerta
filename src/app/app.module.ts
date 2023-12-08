34e42147dca2cc50cd8ac1ec384e1a75ff615111
Patricia_Ortiz <paty090198@gmail.com>
2023-12-07 22:15:54 -0600

    introduccion de libreria primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActualizacionesComponent } from './public/components/pages/rdv/actualizaciones/actualizaciones.component';
+import {CardModule} from "primeng/card";
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { ActualizacionesComponent } from './public/components/pages/rdv/actualiz
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    InputTextModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
