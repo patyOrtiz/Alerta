1e86ab69bd1326f86eba2a282b9b0fd6e7585882
Patricia_Ortiz <paty090198@gmail.com>
2023-12-07 21:01:20 -0600

    proyecto_base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ActualizacionesComponent} from './public/components/pages/rdv/actualizaciones/actualizaciones.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'', component:ActualizacionesComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActualizacionesComponent } from './public/components/pages/rdv/actualizaciones/actualizaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActualizacionesComponent
   ],
   imports: [
     BrowserModule,
